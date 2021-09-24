93a9513427929ef22a14c436ce347d8e796123a3
Yonatan Lavie <yonatan.lavie89@gmail.com>
2021-09-24 19:24:43 +0300

    add signin and signup pages and functionality

--- client/pages/_app.js
+++ client/pages/_app.js
@@ -1,7 +1,12 @@
 import 'bootstrap/dist/css/bootstrap.css';
 
 const _app = ({ Component, pageProps }) => {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <h1>Header!!</h1>
+      <Component {...pageProps} />
+    </>
+  );
 };
 
 export default _app;
--- client/pages/banana.js
+++
@@ -1,5 +0,0 @@
-const banana = () => {
-  return <h1>Banana 2</h1>;
-};
-
-export default banana;
