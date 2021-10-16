4c46dbf7ed2cd4755c0d4beb9e118137b0efbef6
Yonatan Lavie <yonatan.lavie89@gmail.com>
2021-10-16 23:01:07 +0300

    Order service:  - update the common library.  - create expiration complete listener    - get the order id    - update the order status    - publish order cancelled event  - create test cases for expiration complete

---
+++ orders/src/events/listeners/expiration-complete-listener.ts
@@ -0,0 +1,35 @@
+import { Message } from 'node-nats-streaming';
+import { Subjects, Listener, ExpirationCompleteEvent, OrderStatus } from '@ly-common-lib/common';
+import { queueGroupName } from './queue-group-name';
+import { Order } from '../../models/order'
+import { OrderCancelledPublisher } from '../publishers/order-cancelled-publisher';
+
+
+export class ExpirationCompleteListener extends Listener<ExpirationCompleteEvent> {
+    subject: Subjects.ExpirationComplete = Subjects.ExpirationComplete;
+    queueGroupName = queueGroupName;
+
+    async onMessage(data: ExpirationCompleteEvent['data'], msg: Message) {
+        const { orderId } = data;
+
+        const order = await Order.findById(orderId);
+
+        if(!order){
+            throw new Error('Order not found');
+        }
+        
+        order.set({
+            status: OrderStatus.Cancelled,
+        });
+        await order.save();
+        new OrderCancelledPublisher(this.client).publish({
+            id: order.id,
+            version: order.version,
+            ticket: {
+                id: order.ticket.id,
+            },
+        })
+
+        msg.ack();
+    }
+}
